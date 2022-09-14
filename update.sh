5331551df9db637348b4b6c2e0f6aa9375292590
Asysama <1820090402@qq.com>
2022-09-14 14:13:24 +0800

    update hexo backup

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
-info=$1
-if ["$info" = ""];
-then info=":pencil: update content"
-fi
+hexo clean
+hexo d -g
 git add -A
-git commit -m "$info"
+git commit -m 'update hexo backup'
 git push origin hexo
